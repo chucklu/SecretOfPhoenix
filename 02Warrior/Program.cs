8b1da1f4cfc45a6877954cd24f6b514a1b9c9600
Chuck Lu <chucklu2015@outlook.com>
2021-05-07 23:01:40 +0800

    print status

--- 02Warrior/Program.cs
+++ 02Warrior/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,57 @@ namespace SecretOfPhoenix
 {
     class Program
     {
+        private const int maxCount = 3;
+
         static void Main(string[] args)
         {
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Run()
+        {
+
+            int minLeftQuilboarCount = 0;
+            int maxLeftQuilboarCount = maxCount;
+
+            int minLeftOrcCount = 0;
+            int maxLeftOrcCount = maxCount;
+
+            List<Status> list = new List<Status>();
+            int statusCount = 0;
+            for (int i = minLeftQuilboarCount; i <= maxLeftQuilboarCount; i++)
+            {
+                for (int j = minLeftOrcCount; j <= maxLeftOrcCount; j++)
+                {
+                    statusCount++;
+                    var status = new Status()
+                    {
+                        Id = statusCount,
+                        LeftQuilboarCount = i,
+                        RightQuilboarCount = maxCount - i,
+                        LeftOrcCount = j,
+                        RightOrcCount = maxCount - j
+                    };
+
+                    list.Add(status);
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ 02Warrior/Status.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretOfPhoenix
+{
+    public class Status
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 野猪人数量 身材3/3
+        /// </summary>
+        public int LeftQuilboarCount { get; set; }
+
+        public int RightQuilboarCount { get; set; }
+
+        /// <summary>
+        /// 兽人数量 身材1/1
+        /// </summary>
+        public int LeftOrcCount { get; set; }
+
+        public int RightOrcCount { get; set; }
+
+        public bool IsValid()
+        {
+            if (LeftQuilboarCount > LeftOrcCount)
+            {
+                return false;
+            }
+
+            if (RightQuilboarCount > RightOrcCount)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(LeftQuilboarCount);
+            stringBuilder.Append(LeftOrcCount);
+            stringBuilder.Append("LR");
+            stringBuilder.Append(RightQuilboarCount);
+            stringBuilder.Append(RightOrcCount);
+            stringBuilder.Append($" , {Id:D2},isValid = {IsValid()}");
+            return stringBuilder.ToString();
+        }
+    }
+
+}
