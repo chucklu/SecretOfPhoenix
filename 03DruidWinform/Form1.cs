03c959aec7817ca3a84cdce4def365474355ede1
Chuck Lu <chucklu2015@outlook.com>
2021-05-08 23:56:49 +0800

    migrate code from https://www.cnblogs.com/chucklu/p/3969927.html

--- 03DruidWinform/Form1.cs
+++ 03DruidWinform/Form1.cs
@@ -12,9 +12,85 @@ namespace _03DruidWinform
 {
     public partial class Form1 : Form
     {
+        int multiple = 20;
+        int width = 20;
+        int height = 16;
+        int w = 4;
+        int h = 4;
+        Pen bluePen = new Pen(Color.Black);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Console.WriteLine("Form长：{0}，宽：{1}", this.Width, this.Height);
+            this.Multiple();
+            DrawGrid(width, height, w, h, e);
+
+            List<Rectangle> l = new List<Rectangle>();
+            Rectangle r;
+            r = new Rectangle(0, w, 2 * w, h); l.Add(r);
+            r = new Rectangle(3 * w, 0, 2 * w, h); l.Add(r);
+            r = new Rectangle(2 * w, 3 * w, 2 * w, h); l.Add(r);
+            foreach (Rectangle r1 in l)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), r1);
+            }
+        }
+
+        /// <summary>
+        /// 将比例放大
+        /// </summary>
+        private void Multiple()
+        {
+            width = width * multiple;
+            height = height * multiple;
+            w = w * multiple;
+            h = h * multiple;
+        }
+
+        /// <summary>
+        /// 画网格
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="w"></param>
+        /// <param name="h"></param>
+        private void DrawGrid(int width, int height, int w, int h, PaintEventArgs e)
+        {
+            Point p1 = new Point();
+            Point p2 = new Point();
+
+            p1.X = 0; p2.X = width;
+            for (int y = 0; y <= height; y = y + h)
+            {
+                p1.Y = y; p2.Y = y;
+                DrawLine(p1, p2, e);
+            }
+            p1.Y = 0; p2.Y = height;
+            for (int x = 0; x <= width; x = x + w)
+            {
+                p1.X = x; p2.X = x;
+                DrawLine(p1, p2, e);
+            }
+        }
+
+        /// <summary>
+        /// 画直线
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        private void DrawLine(Point p1, Point p2, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(bluePen, p1, p2);
         }
     }
 }
