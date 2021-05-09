7ab45bae7a0275991596e74e48d2f66d404adb6d
Chuck Lu <chucklu2015@outlook.com>
2021-05-09 20:32:24 +0800

    add a new project for hunter

---
+++ 04Hunter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Hunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Run()
+        {
+
+        }
+    }
+}
