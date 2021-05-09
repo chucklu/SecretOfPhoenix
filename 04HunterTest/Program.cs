0236622d74180a69fd8bc59d7176eac7ab48cb91
Chuck Lu <chucklu2015@outlook.com>
2021-05-10 00:25:44 +0800

    add comments for another 7 minions

--- 04HunterTest/Program.cs
+++ 04HunterTest/Program.cs
@@ -28,6 +28,20 @@ namespace _04HunterTest
 
         static void Run()
         {
+            Print1();
+        }
+
+        static void Print1()
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                var cardId = $"BAR_763m{i}";
+                PrintCards(cardId);
+            }
+        }
+
+        static void Print2()
+        {
             for (int i = 1; i <= 7; i++)
             {
                 var cardId = $"BAR_763a{i}";
@@ -37,7 +51,7 @@ namespace _04HunterTest
 
         static void PrintCards(string id)
         {
-           var list= Cards.All.Where(x => x.Value.Id.Contains(id));
+            var list= Cards.All.Where(x => x.Value.Id.Contains(id));
            var cards = list.Select(x => x.Value).ToList();
            foreach (var card in cards)
            {
