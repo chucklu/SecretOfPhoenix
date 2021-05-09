b512cff68e2f438a4e7b8d067471b0e510a0d997
Chuck Lu <chucklu2015@outlook.com>
2021-05-10 00:09:55 +0800

    add test project to get the name and text of task

---
+++ 04HunterTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HearthDb;
+using HearthDb.Enums;
+
+namespace _04HunterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Run()
+        {
+            PrintCards("BAR_763a7");
+        }
+
+        static void PrintCards(string id)
+        {
+           var list= Cards.All.Where(x => x.Value.Id.Contains(id));
+           var cards = list.Select(x => x.Value).ToList();
+           foreach (var card in cards)
+           {
+               var name = card.GetLocName(Locale.zhCN);
+               var text = card.GetLocText(Locale.zhCN);
+               var str = $"//{card.Id} {name}{text}";
+               Console.WriteLine(str);
+           }
+        }
+    }
+}
