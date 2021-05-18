332bb599d5c35dbeb3aff66b7151159c1b505cea
Chuck Lu <chucklu2015@outlook.com>
2021-05-18 19:27:23 +0800

    test the matrix for rougue

--- 01RogueTest/MatrixTest.cs
+++ 01RogueTest/MatrixTest.cs
@@ -15,7 +15,6 @@ namespace _01RogueTest
         public void Test20210518001()
         {
             var M = Matrix<double>.Build;
-            var V = Vector<double>.Build;
 
             // From a 2D-array
             double[,] x = {{ 1.0, 2.0 },
@@ -28,6 +27,39 @@ namespace _01RogueTest
 
             var result = left.Multiply(right);
             Console.WriteLine(result);
+        }
+
+        [Test]
+        public void Test20210518002()
+        {
+            var M = Matrix<double>.Build;
+            var V = Vector<double>.Build;
+
+            var attackArray = new double[] {5, 1, 0, 2, 0, 5, 2};
+            var attackVector = V.DenseOfArray(attackArray);
+
+            var list = new List<Vector<double>>()
+            {
+                V.DenseOfArray(new double[] {1, 2, 1, -2, 2, 0, -3}),//attack the first enemy minion
+                V.DenseOfArray(new double[] {-2, 1, -2, -2, 3, -2, -2}),//attack the second enemy minion
+                V.DenseOfArray(new double[] {0, -2, 1, 1, 2, -2, -2}),
+                V.DenseOfArray(new double[] {0, 0, 1, 1, -2, 3, -3}),
+                V.DenseOfArray(new double[] {-3, 0, -1, 2, 1, -2, 3}),
+                V.DenseOfArray(new double[] {2, 3, 2, -3, 0, 1, 1}),
+                V.DenseOfArray(new double[] {-2, -2, -1, -3, 0, 3, 1}),
+            };
+
+
+            var solutionArray = new double[] {1, 2, 3, 0, 0, 3, 2}; //1*7
+
+            for (int i = 0; i < solutionArray.Length; i++)
+            {
+                var temp = list[i].Multiply(solutionArray[i]);
+                attackVector = attackVector.Add(temp);
+            }
+
+            Console.WriteLine(attackVector);
+
         }
     }
 }
