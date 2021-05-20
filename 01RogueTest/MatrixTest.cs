5899d0647213f6b9c2fe6b4fd7c3d0a176372436
Chuck Lu <chucklu2015@outlook.com>
2021-05-20 18:18:05 +0800

    add IsValid method

--- 01RogueTest/MatrixTest.cs
+++ 01RogueTest/MatrixTest.cs
@@ -59,7 +59,82 @@ namespace _01RogueTest
             }
 
             Console.WriteLine(attackVector);
+            Console.WriteLine(IsValid(attackVector));
 
+        }
+
+        [Test]
+        public void Test20210520001()
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                var variable = $"i{i}";
+                Console.WriteLine($"for (int {variable} = 0; {variable} <= 4; {variable}++)");
+                Console.WriteLine("{");
+            }
+
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine("}");
+            }
+        }
+
+        [Test]
+        public void Test20210520002()
+        {
+            for (int i1 = 0; i1 <= 4; i1++)
+            {
+                for (int i2 = 0; i2 <= 4; i2++)
+                {
+                    for (int i3 = 0; i3 <= 4; i3++)
+                    {
+                        for (int i4 = 0; i4 <= 4; i4++)
+                        {
+                            for (int i5 = 0; i5 <= 4; i5++)
+                            {
+                                for (int i6 = 0; i6 <= 4; i6++)
+                                {
+                                    for (int i7 = 0; i7 <= 4; i7++)
+                                    {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsValid(Vector<double> vector)
+        {
+            bool flag = true;
+            for (int i = 0; i < vector.Count; i++)
+            {
+                var item = vector[i];
+                while (item < 0)
+                {
+                    item += 10;
+                }
+
+                while (item > 10)
+                {
+                    item -= 10;
+                }
+
+                vector[i] = item;
+            }
+
+            var item0 = vector[0];
+            foreach (var item in vector)
+            {
+                if (Math.Abs(item0 - item) > 0.000001)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            return flag;
         }
     }
 }
